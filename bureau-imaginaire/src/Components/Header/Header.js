c5617a74ebf7d8b4d4d91d0809cd3f5860f82a90
max-machin <machin.max@lapalteforme.io>
2022-07-06 14:39:23 +0200

    Test d'import de photo

--- bureau-imaginaire/src/Components/Header/Header.js
+++ bureau-imaginaire/src/Components/Header/Header.js
@@ -1,10 +1,16 @@
 import * as React from "react";
+import {Link} from "react-router-dom";
+import image from "public/logo_carre_BI.png"
 
 export function Header (){
     return (
         <div>
             <ul>
-                <li>Salut</li>
+                <li><Link to="/NosRealisations">Nos réalisations</Link></li>
+                <li><Link to="/APropos">Qui sommes-nous?</Link></li>
+                <li><Link to="/"><img src={image} alt="Logo"></img></Link></li>
+                <li><Link to="/TravaillerEnsemble">Travailler ensemble</Link></li>
+                <li><Link to="/Devis">Devis</Link></li>
             </ul>
         </div>
     )
--- bureau-imaginaire/src/Pages/Home.js
+++ bureau-imaginaire/src/Pages/Home.js
@@ -1,12 +1,11 @@
 import * as React from "react";
 import {Header} from "../Components/Header/Header.js"
-import {Link} from "react-router-dom";
+
 
 export function Home() {
     return (
       <div>
         <Header />
-        <Link to="/about">About</Link>
       </div>
     )
 }
