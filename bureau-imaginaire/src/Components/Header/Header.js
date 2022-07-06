e339db706f37e54e37d194645c8d9a9b124ffff9
max-machin <machin.max@lapalteforme.io>
2022-07-06 14:52:42 +0200

    Mise en place du header

--- bureau-imaginaire/src/Components/Header/Header.js
+++ bureau-imaginaire/src/Components/Header/Header.js
@@ -1,14 +1,16 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
-import image from "public/logo_carre_BI.png"
+import image from "../../img/logo_carre_BI.png";
+import './Header.css';
 
 export function Header (){
     return (
+        
         <div>
             <ul>
                 <li><Link to="/NosRealisations">Nos réalisations</Link></li>
                 <li><Link to="/APropos">Qui sommes-nous?</Link></li>
-                <li><Link to="/"><img src={image} alt="Logo"></img></Link></li>
+                <li><Link to="/"><img src={image} alt="Logo" width="200"></img></Link></li>
                 <li><Link to="/TravaillerEnsemble">Travailler ensemble</Link></li>
                 <li><Link to="/Devis">Devis</Link></li>
             </ul>
